d59745a6c5b890f68c4299f30baeb8ac7163ed9c
devnpl <devnpl@hotmail.com>
2018-03-15 06:39:33 +0100

    Add project files.

---
+++ Properties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class personWithCountry
+    {
+        private string _name;
+        private string _lastName="Nepal";
+
+        public string Name { get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+                }
+        public string printFullName()
+        {
+            var fullName = Name + " " + _lastName;
+            Console.WriteLine(fullName);
+            return fullName;
+        }
+
+        static void Main(string[] args)
+        {
+            personWithCountry Nepalese = new personWithCountry();
+            Nepalese.Name = "Dev";
+            Nepalese.printFullName();
+
+            Console.ReadKey();
+        }
+    }
+}
